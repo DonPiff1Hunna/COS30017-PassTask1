e004eef0715245b7283c98586ba557ede725866c
DonPiff1Hunna <chris.donaldson1@hotmail.com>
2023-08-16 17:21:50 +1000

    edits

--- app/src/main/java/au/edu/swin/sdmd/thecontest/MainActivity.kt
+++ app/src/main/java/au/edu/swin/sdmd/thecontest/MainActivity.kt
@@ -1,6 +1,7 @@
 package au.edu.swin.sdmd.thecontest
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Button
@@ -11,11 +12,12 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
+    var value = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var value = 0
+
         val score = findViewById<Button>(R.id.score)
         val reset = findViewById<Button>(R.id.reset)
         val steal = findViewById<Button>(R.id.steal)
@@ -36,8 +38,19 @@ class MainActivity : AppCompatActivity() {
             currentScore.text = value.toString()
         }
 
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
 
-
+        val userNumber = value
+        outState.putInt("savedInt", value)
     }
 
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val currentScore = findViewById<TextView>(R.id.currentScore)
+        val userInt = savedInstanceState.getInt("savedInt", 0)
+        value = userInt
+        currentScore.text = value.toString()
+    }
 }
